3b2bc9205dd48bb3c475592f48b78ef9871049a2
Axel <axelbaxel@gmail.com>
2019-05-29 17:47:00 +0200

    Added some more CSS

--- src/NewScan.js
+++ src/NewScan.js
@@ -1,28 +1,30 @@
 import React from 'react';
+import './newScan.css'
 
 const NewScan = (props) => { 
 
         return (
             <div >
                 <form onSubmit={props.addNewScan} className='newScan'>
-                    <div id="newName">
-                        Scan Name:
+                    <div id="newName" className="inputPart">
+                        <label>Scan Name:</label>
+
                         <input placeholder="Item name" type='text' value={props.newName} onChange={props.handleNameChange}></input>
                     </div>
-                    <div id="newUser">
-                        Scanning User:
+                    <div id="newUser" className="inputPart">
+                    <label>Scanning User:</label>
                         <select value={props.newUser} onChange={props.handleUserChange}>
                             {props.users.map(user => (
                                 <option key={user.id} value={user.id}>{user.name}</option>
                             ))}   
                         </select>
                     </div>
-                    <div id="newMinEle">
-                        Elevation Minimum:
+                    <div id="newMinEle" className="inputPart">
+                    <label>Elevation Minimum:</label>
                         <input type='number' value={props.newMinEle} onChange={props.handleMinEleChange}></input>
                     </div>
-                    <div id="newMaxEle">
-                        Elevation Maximum:
+                    <div id="newMaxEle" className="inputPart">
+                    <label>Elevation Maximum:</label>
                         <input type='number' value={props.newMaxEle} onChange={props.handleMaxEleChange}></input>
                     </div>
                     <input type='submit' value="Add Scan" disabled={props.newName.length !== 0 && props.newMinEle < props.newMaxEle ? false : true}></input>
